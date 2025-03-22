1f7985946d4897bc0ba23898b29fc79166f4b368
helium-dimer <helio.junior2802@outlook.com>
2025-03-21 21:04:47 -0300

    Update batalhaNaval.c
    isso me lembra de quando eu desenvolvia jogos na Unity Engine, legal!

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,40 +1,110 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
+#define TAMANHO 10
+#define NAVIO 3
+#define ATINGIDO 1
+#define AGUA 0
 
-int main() {
-    // Nível Novato - Posicionamento dos Navios
-    // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
-    // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
-    // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+int tabuleiro[TAMANHO][TAMANHO];
 
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
+void inicializarTabuleiro() 
+{
+    for (int i = 0; i < TAMANHO; i++) 
+    {
+        for (int j = 0; j < TAMANHO; j++) 
+        {
+            tabuleiro[i][j] = AGUA;
+        }
+    }
+}
 
-    // Nível Mestre - Habilidades Especiais com Matrizes
-    // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
-    // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
+void posicionarNavio(int x, int y, int tamanho, char direcao) 
+{
+    for (int i = 0; i < tamanho; i++) 
+    {
+        if (direcao == 'H') 
+        {
+            tabuleiro[x][y + i] = NAVIO;
+        } 
+        else if (direcao == 'V') 
+        {
+            tabuleiro[x + i][y] = NAVIO;
+        } 
+        else if (direcao == 'D') 
+        {
+            tabuleiro[x + i][y + i] = NAVIO;
+        }
+    }
+}
 
-    // Exemplos de exibição das habilidades:
-    // Exemplo para habilidade em cone:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 1 1 1 1 1
+void exibirTabuleiro() 
+{
+    printf("\nTabuleiro:\n");
+    for (int i = 0; i < TAMANHO; i++) 
+    {
+        for (int j = 0; j < TAMANHO; j++) 
+        {
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void aplicarHabilidade(char tipo, int x, int y) 
+{
+    int impacto[TAMANHO][TAMANHO] = {0};
     
-    // Exemplo para habilidade em octaedro:
-    // 0 0 1 0 0
-    // 0 1 1 1 0
-    // 0 0 1 0 0
+    if (tipo == 'C') 
+    {
+        for (int i = 0; i < TAMANHO; i++) 
+        {
+            impacto[x][i] = ATINGIDO;
+            impacto[i][y] = ATINGIDO;
+        }
+    } 
+    else if (tipo == 'O') 
+    {
+        if (x > 0) impacto[x-1][y] = ATINGIDO;
+        if (x < TAMANHO-1) impacto[x+1][y] = ATINGIDO;
+        if (y > 0) impacto[x][y-1] = ATINGIDO;
+        if (y < TAMANHO-1) impacto[x][y+1] = ATINGIDO;
+    } 
+    else if (tipo == 'N') 
+    {
+        if (x > 0) impacto[x-1][y] = ATINGIDO;
+        if (x > 1) impacto[x-2][y] = ATINGIDO;
+        if (y > 0 && x > 0) impacto[x-1][y-1] = ATINGIDO;
+        if (y < TAMANHO-1 && x > 0) impacto[x-1][y+1] = ATINGIDO;
+    }
+    
+    printf("\nHabilidade aplicada (%c) no ponto (%d, %d):\n", tipo, x, y);
+    for (int i = 0; i < TAMANHO; i++) 
+    {
+        for (int j = 0; j < TAMANHO; j++) 
+        {
+            printf("%d ", impacto[i][j]);
+        }
+        printf("\n");
+    }
+}
 
-    // Exemplo para habilidade em cruz:
-    // 0 0 1 0 0
-    // 1 1 1 1 1
-    // 0 0 1 0 0
-
+int main() 
+{
+    srand(time(NULL));
+    inicializarTabuleiro();
+    
+    posicionarNavio(2, 2, 4, 'H');
+    posicionarNavio(5, 3, 3, 'V');
+    posicionarNavio(1, 1, 3, 'D');
+    posicionarNavio(6, 6, 2, 'D');
+    
+    exibirTabuleiro();
+    
+    aplicarHabilidade('C', 5, 5);
+    aplicarHabilidade('O', 3, 3);
+    aplicarHabilidade('N', 7, 7);
+    
     return 0;
 }
